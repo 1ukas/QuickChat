8a2fb8d5ff90dd3c1a46d45f9ef518c3c595fbf0
Lukas <miltenil@coventry.ac.uk>
2019-06-03 16:47:55 +0300

    FileProvider Setup and Video Capture Activity

--- app/src/main/java/com/miltenil/quickchat/Fragments/MenuFragment.java
+++ app/src/main/java/com/miltenil/quickchat/Fragments/MenuFragment.java
@@ -2,6 +2,7 @@ package com.miltenil.quickchat.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.miltenil.quickchat.Activities.FriendsListActivity;
 import com.miltenil.quickchat.Activities.LogInActivity;
 import com.miltenil.quickchat.Activities.ProfileActivity;
+import com.miltenil.quickchat.Activities.VideoCaptureActivity;
 import com.miltenil.quickchat.R;
 import com.miltenil.quickchat.Utils.AddFriend;
 
@@ -47,44 +49,65 @@ public class MenuFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.new_post_menu:
-                Toast.makeText(getActivity(), "New Post", Toast.LENGTH_SHORT).show();
+                HandleNewPostItem();
                 return true;
             case R.id.add_friend_menu:
-                final EditText addFriendField = getView().findViewById(R.id.addFriendNameInput);
-                final Button addFriendButton = getView().findViewById(R.id.addFriendNameBtn);
-                addFriendField.setVisibility(View.VISIBLE);
-                addFriendButton.setVisibility(View.VISIBLE);
-                addFriendButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        final String addFriendString = addFriendField.getText().toString();
-                        if (!addFriendString.isEmpty()) {
-                            new AddFriend(FirebaseFirestore.getInstance()).FindFriend(addFriendString, mAuth.getUid());
-                            Toast.makeText(getActivity(), "Friend Added Successfully", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(getActivity(), "Invalid Details", Toast.LENGTH_SHORT).show();
-                        }
-                        addFriendField.setVisibility(View.GONE);
-                        addFriendButton.setVisibility(View.GONE);
-                    }
-                });
+                HandleAddFriendItem();
                 return true;
             case R.id.friends_list_menu:
-                Intent intentFriends = new Intent(getActivity(), FriendsListActivity.class);
-                startActivity(intentFriends);
+                HandleFriendsListItem();
                 return true;
             case R.id.profile_menu:
-                Intent intentProfile = new Intent(getActivity(), ProfileActivity.class);
-                startActivity(intentProfile);
+                HandleProfileItem();
                 return true;
             case R.id.logout_menu:
-                FirebaseAuth.getInstance().signOut(); // Log user out
-                Intent intentLogin = new Intent(getActivity(), LogInActivity.class);
-                startActivity(intentLogin); // Open LogInActivity after logging user out
+                HandleLogoutItem();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void HandleNewPostItem() {
+        Intent intent = new Intent(getActivity(), VideoCaptureActivity.class);
+        startActivity(intent);
+    }
+
+    private void HandleAddFriendItem() {
+        final EditText addFriendField = getView().findViewById(R.id.addFriendNameInput);
+        final Button addFriendButton = getView().findViewById(R.id.addFriendNameBtn);
+        addFriendField.setVisibility(View.VISIBLE);
+        addFriendButton.setVisibility(View.VISIBLE);
+        addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String addFriendString = addFriendField.getText().toString();
+                if (!addFriendString.isEmpty()) {
+                    new AddFriend(FirebaseFirestore.getInstance()).FindFriend(addFriendString, mAuth.getUid());
+                    Toast.makeText(getActivity(), "Friend Added Successfully", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getActivity(), "Invalid Details", Toast.LENGTH_SHORT).show();
+                }
+                addFriendField.setVisibility(View.GONE);
+                addFriendButton.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void HandleFriendsListItem() {
+        Intent intent = new Intent(getActivity(), FriendsListActivity.class);
+        startActivity(intent);
+    }
+
+    private void HandleProfileItem() {
+        Intent intent = new Intent(getActivity(), ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    private void HandleLogoutItem() {
+        FirebaseAuth.getInstance().signOut(); // Log user out
+        Intent intent = new Intent(getActivity(), LogInActivity.class);
+        startActivity(intent); // Open LogInActivity after logging user out
     }
 }
